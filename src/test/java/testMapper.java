aca4d8c06ebdeb2e212c043ea700983684c84d75
perpig <1350575461@qq.com>
2025-02-06 18:18:30 +0800

    测试usermapper

---
+++ src/test/java/testMapper.java
@@ -0,0 +1,34 @@
+import com.shu.mapper.StudentMapper;
+import com.shu.pojo.Student;
+import com.shu.util.SqlSessionFactoryUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+
+import java.io.PrintWriter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class testMapper {
+    @Test
+    public void testStudentMapper() {
+        String id = "1";
+        String password = "123";
+
+        // 2.调用MyBatis完成查询
+        // 这里直接去官网复制粘贴过来
+        // 2.1 获取SqlSessionFactory对象 优化以后用了工具类 这样只创建一个工厂
+        SqlSessionFactory sqlSessionFactory = SqlSessionFactoryUtils.getSqlSessionFactory();
+        // 2.2 获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 2.3 获取Mapper
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+        // 2.4 调用方法
+        Student student = studentMapper.selectById(Integer.parseInt(id));
+
+        System.out.println(student);
+    }
+
+
+}
