abac7401ce6deb4403a8e1d31d9677445ad85391
perpig <1350575461@qq.com>
2025-02-06 18:23:06 +0800

    学生教师mapper测试通过

--- src/main/java/com/shu/web/LoginServlet.java
+++ src/main/java/com/shu/web/LoginServlet.java
@@ -1,6 +1,9 @@
 package com.shu.web;
 
+import com.shu.mapper.StudentMapper;
+import com.shu.mapper.TeacherMapper;
 import com.shu.pojo.Student;
+import com.shu.pojo.Teacher;
 import com.shu.util.SqlSessionFactoryUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -32,7 +35,70 @@ public class LoginServlet extends HttpServlet {
         SqlSessionFactory sqlSessionFactory = SqlSessionFactoryUtils.getSqlSessionFactory();
         // 2.2 获取SqlSession对象
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        // 2.3 获取Mapper
+
+        // 获取字符输出流,并设置content type
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter writer = resp.getWriter();
+
+
+        if (role.equals("student")) {
+            // 2.3 获取Mapper
+            StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+            // 2.4 调用方法
+            Student student = studentMapper.selectById(Integer.parseInt(id));
+
+            if (student.getPassword().equals(password)) {
+                // 成功
+
+                // 存入session
+                HttpSession session = req.getSession();
+                session.setAttribute("student", student);
+
+                // 登陆成功，跳转到主页或其他页面
+                writer.write("<html><body>");
+                writer.write("<h1>登录成功！</h1>");
+                writer.write("<script>window.alert(\"登陆成功\");</script>");
+                writer.write("<script>window.location.href='home.html';</script>"); // 登录成功后跳转
+                writer.write("</body></html>");
+            } else {
+                // 登陆失败，弹出提示框
+                writer.write("<html><body>");
+                writer.write("<h1>登录失败！</h1>");
+                writer.write("<script>alert('Id或密码错误！'); window.history.back();</script>"); // 弹窗并返回上一步
+                writer.write("</body></html>");
+            }
+
+
+        }
+        else if (role.equals("teacher")) {
+            // 2.3 获取Mapper
+            TeacherMapper teacherMapper = sqlSession.getMapper(TeacherMapper.class);
+            // 2.4 调用方法
+            Teacher teahcer = teacherMapper.selectById(Integer.parseInt(id));
+            if (teahcer.getPassword().equals(password)) {
+                // 成功
+
+                // 存入session
+                HttpSession session = req.getSession();
+                session.setAttribute("teacher", teahcer);
+
+                // 登陆成功，跳转到主页或其他页面
+                writer.write("<html><body>");
+                writer.write("<h1>登录成功！</h1>");
+                writer.write("<script>window.alert(\"登陆成功\");</script>");
+                writer.write("<script>window.location.href='home.html';</script>"); // 登录成功后跳转
+                writer.write("</body></html>");
+            } else {
+                // 登陆失败，弹出提示框
+                writer.write("<html><body>");
+                writer.write("<h1>登录失败！</h1>");
+                writer.write("<script>alert('Id或密码错误！'); window.history.back();</script>"); // 弹窗并返回上一步
+                writer.write("</body></html>");
+            }
+        }
+
+        // 2.5 释放资源
+        sqlSession.close();
 
     }
 
--- src/test/java/testMapper.java
+++ src/test/java/testMapper.java
@@ -1,5 +1,7 @@
 import com.shu.mapper.StudentMapper;
+import com.shu.mapper.TeacherMapper;
 import com.shu.pojo.Student;
+import com.shu.pojo.Teacher;
 import com.shu.util.SqlSessionFactoryUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -30,5 +32,24 @@ public class testMapper {
         System.out.println(student);
     }
 
+    @Test
+    public void testTeacherMapper() {
+        String id = "1";
+        String password = "123";
+
+        // 2.调用MyBatis完成查询
+        // 这里直接去官网复制粘贴过来
+        // 2.1 获取SqlSessionFactory对象 优化以后用了工具类 这样只创建一个工厂
+        SqlSessionFactory sqlSessionFactory = SqlSessionFactoryUtils.getSqlSessionFactory();
+        // 2.2 获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 2.3 获取Mapper
+        TeacherMapper teacherMapper = sqlSession.getMapper(TeacherMapper.class);
+        // 2.4 调用方法
+        Teacher teacher = teacherMapper.selectById(Integer.parseInt(id));
+
+        System.out.println(teacher);
+    }
 
 }
