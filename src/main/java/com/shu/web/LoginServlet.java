3c2a81eee3170380eb29a9506e67095c0cf073be
perpig <1350575461@qq.com>
2025-02-06 17:25:42 +0800

    登录处理请求的servlet

---
+++ src/main/java/com/shu/web/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.shu.web;
+
+import com.shu.pojo.Student;
+import com.shu.util.SqlSessionFactoryUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 设置请求的字符编码为UTF-8,不然会出乱码
+        req.setCharacterEncoding("UTF-8");
+
+        // 获取分类：学生还是教师
+        String role = req.getParameter("role");
+
+        // 1.接收用户名和密码
+        String id = req.getParameter("id");
+        String password = req.getParameter("password");
+
+        // 2.调用MyBatis完成查询
+        // 这里直接去官网复制粘贴过来
+        // 2.1 获取SqlSessionFactory对象 优化以后用了工具类 这样只创建一个工厂
+        SqlSessionFactory sqlSessionFactory = SqlSessionFactoryUtils.getSqlSessionFactory();
+        // 2.2 获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 2.3 获取Mapper
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
