0840702add5280d14a0f3ae3312fd867ab250779
perpig <1350575461@qq.com>
2025-02-06 22:31:56 +0800

    课程搜索的结果类

---
+++ src/main/java/com/shu/dto/CourseSearchDTO.java
@@ -0,0 +1,110 @@
+package com.shu.dto;
+
+public class CourseSearchDTO {
+    private Integer courseId;          // 课程号
+    private String courseName;         // 课程名
+    private Integer credit;            // 学分
+    private Integer teacherId;         // 教师号
+    private String teacherName;        // 教师姓名
+    private String position;           // 教师职称
+    private String classTime;          // 上课时间
+    private String classroom;          // 教室
+    private Integer capacity;          // 课容量
+    private Integer enrolledCount;     // 已选人数
+
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public Integer getCredit() {
+        return credit;
+    }
+
+    public void setCredit(Integer credit) {
+        this.credit = credit;
+    }
+
+    public Integer getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Integer teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getClassTime() {
+        return classTime;
+    }
+
+    public void setClassTime(String classTime) {
+        this.classTime = classTime;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public Integer getEnrolledCount() {
+        return enrolledCount;
+    }
+
+    public void setEnrolledCount(Integer enrolledCount) {
+        this.enrolledCount = enrolledCount;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseSearchDTO{" +
+                "courseId=" + courseId +
+                ", courseName='" + courseName + '\'' +
+                ", credit=" + credit +
+                ", teacherId=" + teacherId +
+                ", teacherName='" + teacherName + '\'' +
+                ", position='" + position + '\'' +
+                ", classTime='" + classTime + '\'' +
+                ", classroom='" + classroom + '\'' +
+                ", capacity=" + capacity +
+                ", enrolledCount=" + enrolledCount +
+                '}';
+    }
+}
