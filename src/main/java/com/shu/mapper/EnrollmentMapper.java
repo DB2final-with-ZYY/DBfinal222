14a1edec6d850cf6c8239c5e07d397f1488683ee
perpig <1350575461@qq.com>
2025-02-07 15:07:03 +0800

    插入选课的方法

--- src/main/java/com/shu/mapper/EnrollmentMapper.java
+++ src/main/java/com/shu/mapper/EnrollmentMapper.java
@@ -5,5 +5,12 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface EnrollmentMapper {
+
+    /**
+     * 插入选课情况 要填充student_id 和schedule_id
+     * @param enrollment
+     * @return
+     */
+    int insert(Enrollment enrollment);
     
 } 
