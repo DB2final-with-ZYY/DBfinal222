7045dc6b4283eda7beab0de45f1e58e02eb3fa0f
perpig <1350575461@qq.com>
2025-02-07 15:32:18 +0800

    获取已选人数的方法

--- src/main/java/com/shu/mapper/EnrollmentMapper.java
+++ src/main/java/com/shu/mapper/EnrollmentMapper.java
@@ -8,9 +8,17 @@ public interface EnrollmentMapper {
 
     /**
      * 插入选课情况 要填充student_id 和schedule_id
+     * 
      * @param enrollment
      * @return
      */
     int insert(Enrollment enrollment);
-    
-} 
+
+    /**
+     * 获取某个课程的已选人数
+     * 
+     * @param scheduleId 课程安排ID
+     * @return 已选人数
+     */
+    int getEnrolledCount(Integer scheduleId);
+}
