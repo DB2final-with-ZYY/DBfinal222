84bd024bf4dfafd2ff2e979b1f5eae9159043412
perpig <1350575461@qq.com>
2025-02-07 15:07:51 +0800

    查询专业的方法

--- src/main/java/com/shu/mapper/MajorMapper.java
+++ src/main/java/com/shu/mapper/MajorMapper.java
@@ -5,5 +5,11 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MajorMapper {
+    /**
+     * 根据id查专业
+     * @param MajorId
+     * @return
+     */
+    Major selectMajorById(int MajorId);
     
 } 
