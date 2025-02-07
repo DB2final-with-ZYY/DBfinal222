f3bc01b92ec4f1dcd3a5e1af15ed39e6e240110c
perpig <1350575461@qq.com>
2025-02-07 18:45:44 +0800

    查询某专业的选课计划

--- src/main/java/com/shu/mapper/CoursePlanMapper.java
+++ src/main/java/com/shu/mapper/CoursePlanMapper.java
@@ -1,9 +1,21 @@
 package com.shu.mapper;
 
+import com.shu.dto.CourseSearchDTO;
+import com.shu.pojo.Course;
 import com.shu.pojo.CoursePlan;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface CoursePlanMapper {
+
+    /**
+     * 根据专业id查课程信息
+     * @param majorId
+     * @return
+     */
+    List<Course> searchCoursePlanByMajor(@Param("majorId") Integer majorId);
     
 } 
