a531ead389f0d77fa16a5ba6f8bff87efbb280d1
perpig <1350575461@qq.com>
2025-02-09 20:32:19 +0800

    更新和修改成绩的mapper

--- src/main/java/com/shu/mapper/GradeMapper.java
+++ src/main/java/com/shu/mapper/GradeMapper.java
@@ -5,5 +5,18 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface GradeMapper {
+    /**
+     * 更新成绩 需要enrollment_id grade
+     * @param grade
+     * @return
+     */
+    int update(Grade grade);
+
+    /**
+     * 插入成绩 需要enrollment_id grade
+     * @param grade
+     * @return
+     */
+    int insert(Grade grade);
     
 } 
