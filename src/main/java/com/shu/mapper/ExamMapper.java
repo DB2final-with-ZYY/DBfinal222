3a96777ffa83fd62a97aef32450e8818f60c9aae
perpig <1350575461@qq.com>
2025-02-09 19:08:24 +0800

    添加考试的mapper方法

--- src/main/java/com/shu/mapper/ExamMapper.java
+++ src/main/java/com/shu/mapper/ExamMapper.java
@@ -5,4 +5,10 @@ import javax.servlet.annotation.WebServlet;
 
 @WebServlet
 public interface ExamMapper {
+    /**
+     * 插入考试 需要schedule_id exam_time exam_place
+     * @param exam
+     * @return
+     */
+    int insert(Exam exam);
 }
