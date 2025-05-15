aaa23b6c60b22788fbd98b048fe776d7ab4b97ab
perpig <1350575461@qq.com>
2025-05-15 20:11:56 +0800

    创建课程时添加考试占比

--- src/main/java/com/shu/web/CreateCourseServlet.java
+++ src/main/java/com/shu/web/CreateCourseServlet.java
@@ -19,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,11 +45,13 @@ public class CreateCourseServlet extends HttpServlet {
         String courseName = req.getParameter("courseName");
         String departmentId = req.getParameter("department");
         String credit = req.getParameter("credit");
+        String examWeight = req.getParameter("examWeight");
 
         System.out.println("/createCourse");
         System.out.println("courseName: " + courseName);
         System.out.println("departmentId: " + departmentId);
         System.out.println("credit: " + credit);
+        System.out.println("examWeight: " + examWeight);
 
 
         // 获取SqlSession
@@ -65,6 +68,7 @@ public class CreateCourseServlet extends HttpServlet {
             course.setCourseName(courseName);
             course.setCredit(Integer.parseInt(credit));
             course.setDepartmentId(Integer.parseInt(departmentId));
+            course.setExamWeight(new BigDecimal(examWeight));
 
             // 插入
             CourseMapper courseMapper = sqlSession.getMapper(CourseMapper.class);
