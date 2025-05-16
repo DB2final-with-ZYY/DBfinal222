98bf2158506600275df9e4b31f54e7809b919ebd
perpig <1350575461@qq.com>
2025-05-16 09:06:48 +0800

    专业增加按照学院名字和id查

--- src/main/java/com/shu/mapper/MajorMapper.java
+++ src/main/java/com/shu/mapper/MajorMapper.java
@@ -2,6 +2,8 @@ package com.shu.mapper;
 
 import com.shu.pojo.Major;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
 
 @Mapper
 public interface MajorMapper {
@@ -11,5 +13,20 @@ public interface MajorMapper {
      * @return
      */
     Major selectMajorById(int MajorId);
-    
-} 
+
+
+    /**
+     * 根据院系名称查询专业列表
+     * @param departmentName 院系名称
+     * @return 专业列表
+     */
+    List<Major> selectMajorsByDepartment(String departmentName);
+
+    /**
+     * 根据院系ID查询专业列表
+     * @param departmentId 院系ID
+     * @return 专业列表
+     */
+    List<Major> selectMajorsByDepartmentId(@Param("departmentId") int departmentId);
+
+}
